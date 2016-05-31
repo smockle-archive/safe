9ce7e71e60bc3ac3b8e1e53a45504e67312b7e05
Clay Miller <clay@smockle.com>
2016-05-30 17:20:06 -0700

    Rearrange tests and achieve full coverage.

--- test/safe.js
+++ test/safe.js
@@ -3,8 +3,22 @@ const test = require('tape')
 const safe = require('../lib/safe')
 
 test('safe', (t) => {
-  t.plan(5)
+  t.plan(6)
 
+  // Case 0
+  const check = safe()
+  const divide = check()(
+    [
+      { name: 'a', type: 'number' },
+      { name: 'b', type: 'number' }
+    ])(
+      function (a /* : number */, b /* : number */) /* : number */ {
+        return a / b
+      }
+    )
+  t.equal(divide(6, 2), 3, 'noop')
+
+  // Case 1
   const types = safe(
     [
       { name: 'a', type: 'number' },
@@ -21,6 +35,11 @@ test('safe', (t) => {
       return a - b
     }
   )
+  t.throws(add.bind(add, '3', 1), TypeError, 'throws typeerror')
+  t.equal(add(3, 1), 4, 'partially apply types')
+  t.equal(subtract(4, 2), 2, 're-use partially-applied types')
+
+  // Case 2
   const multiply = safe(
     [
       { name: 'a', type: 'number' },
@@ -30,6 +49,9 @@ test('safe', (t) => {
       return a * b
     }
   )
+  t.equal(multiply(2, 3), 6, 'partially apply types and function')
+
+  // Case 3
   const sum = safe(
     [
       { name: 'a', type: 'number' },
@@ -41,10 +63,5 @@ test('safe', (t) => {
     3,
     4
   )
-
-  t.throws(add.bind(add, '3', 1), TypeError, 'throws typeerror')
-  t.equal(add(3, 1), 4, 'partially apply types')
-  t.equal(subtract(4, 2), 2, 're-use partially-applied types')
-  t.equal(multiply(2, 3), 6, 'partially apply types and function')
   t.equal(sum, 7, 'fully apply types and function and values')
 })
