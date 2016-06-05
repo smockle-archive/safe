8dd0990d1fc484387a1122549784cd3efdb39e85
Clay Miller <clay@smockle.com>
2016-06-05 16:29:18 -0700

    Add wrapperless version of safe that can be inserted as the first line in functions. Also comprehend both primitive and instantiable types.

--- test/safe.js
+++ test/safe.js
@@ -1,67 +1,21 @@
 // @flow
 const test = require('tape')
-const safe = require('../lib/safe')
+const { safe } = require('../lib/safe')
 
-test('safe', (t) => {
-  t.plan(6)
+test('safe.safe', (t) => {
+  t.plan(2)
 
-  // Case 0
-  const check = safe()
-  const divide = check()(
-    [
+  function add (a /* : number */, b /* : number */) /* : number */ {
+    const err = safe([
       { name: 'a', type: 'number' },
       { name: 'b', type: 'number' }
-    ])(
-      function (a /* : number */, b /* : number */) /* : number */ {
-        return a / b
-      }
-    )
-  t.equal(divide(6, 2), 3, 'noop')
+    ], arguments)
+    if (err) throw err
 
-  // Case 1
-  const types = safe(
-    [
-      { name: 'a', type: 'number' },
-      { name: 'b', type: 'number' }
-    ]
-  )
-  const add = types(
-    function (a /* : number */, b /* : number */) /* : number */ {
-      return a + b
-    }
-  )
-  const subtract = types(
-    function (a /* : number */, b /* : number */) /* : number */ {
-      return a - b
-    }
-  )
-  t.throws(add.bind(add, '3', 1), TypeError, 'throws typeerror')
-  t.equal(add(3, 1), 4, 'partially apply types')
-  t.equal(subtract(4, 2), 2, 're-use partially-applied types')
+    return a + b
+  }
 
-  // Case 2
-  const multiply = safe(
-    [
-      { name: 'a', type: 'number' },
-      { name: 'b', type: 'number' }
-    ],
-    function (a /* : number */, b /* : number */) /* : number */ {
-      return a * b
-    }
-  )
-  t.equal(multiply(2, 3), 6, 'partially apply types and function')
-
-  // Case 3
-  const sum = safe(
-    [
-      { name: 'a', type: 'number' },
-      { name: 'b', type: 'number' }
-    ],
-    function (a /* : number */, b /* : number */) /* : number */ {
-      return a + b
-    },
-    3,
-    4
-  )
-  t.equal(sum, 7, 'fully apply types and function and values')
+  t.equal(add(1, 2), 3, 'adds numbers')
+  // $flow_disable_line
+  t.throws(add.bind(add, '', 2), TypeError('a must be a number'), 'throws TypeError')
 })
