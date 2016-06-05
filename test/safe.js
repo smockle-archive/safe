33cdd9e631107ff27e3ed4cf051a881eccec0c12
Clay Miller <clay@smockle.com>
2016-06-05 16:40:34 -0700

    Add test coverage for safe.safe (instantiable type).

--- test/safe.js
+++ test/safe.js
@@ -2,7 +2,7 @@
 const test = require('tape')
 const { safe } = require('../lib/safe')
 
-test('safe.safe', (t) => {
+test('safe.safe (primitive type)', (t) => {
   t.plan(2)
 
   function add (a /* : number */, b /* : number */) /* : number */ {
@@ -18,4 +18,22 @@ test('safe.safe', (t) => {
   t.equal(add(1, 2), 3, 'adds numbers')
   // $flow_disable_line
   t.throws(add.bind(add, '', 2), TypeError('a must be a number'), 'throws TypeError')
+})
+
+test('safe.safe (instantiable type)', (t) => {
+  t.plan(2)
+
+  function add (a /* : Array<number> */, b /* : number */) /* : number */ {
+    const err = safe([
+      { name: 'a', type: 'array' },
+      { name: 'b', type: 'number' }
+    ], arguments)
+    if (err) throw err
+
+    return a[0] + b
+  }
+
+  t.equal(add([1], 2), 3, 'adds numbers')
+  // $flow_disable_line
+  t.throws(add.bind(add, '', 2), TypeError('a must be a array'), 'throws TypeError')
 })
