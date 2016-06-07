f9c2d0c78c652bcdbf69738ad93cec5e5b4181b2
Clay Miller <clay@smockle.com>
2016-06-06 23:44:53 -0700

    Add test coverage for safe.safe (nonexisting type).

--- test/safe.js
+++ test/safe.js
@@ -37,3 +37,20 @@ test('safe.safe (instantiable type)', (t) => {
   // $flow_disable_line
   t.throws(add.bind(add, '', 2), TypeError('a must be a array'), 'throws TypeError')
 })
+
+test('safe.safe (nonexisting type)', (t) => {
+  t.plan(1)
+
+  function add (a /* : number */, b /* : number */) /* : number */ {
+    const err = safe([
+      { name: 'a', type: 'zilch' },
+      { name: 'b', type: 'number' }
+    ], arguments)
+    if (err) throw err
+
+    return a + b
+  }
+
+  // $flow_disable_line
+  t.throws(add.bind(add, '', 2), TypeError('a must be a zilch'), 'throws TypeError')
+})
